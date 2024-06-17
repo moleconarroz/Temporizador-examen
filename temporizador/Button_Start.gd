f283cb4e3950ebfa18eb540e8c17d7dc6ed5d31d
Erick <loco.erms@gmail.com>
2024-06-17 09:34:59 -0600

    profe oholi

---
+++ temporizador/Button_Start.gd
@@ -0,0 +1,13 @@
+extends Button
+
+signal start_timer_btn
+
+var minutes = 1
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ temporizador/node_2d.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+var minutes = 1
+var seconds = 0
+var is_running = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
