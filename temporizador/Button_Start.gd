132ca0ac557124914ac13677fc91066302487b2f
Erick <loco.erms@gmail.com>
2024-06-18 14:28:02 -0600

    examen de info

--- temporizador/Button_Start.gd
+++ temporizador/Button_Start.gd
@@ -1,13 +1,1 @@
-extends Button
 
-signal start_timer_btn
-
-var minutes = 1
-
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- temporizador/node_2d.gd
+++ temporizador/node_2d.gd
@@ -1,15 +1,103 @@
 extends CanvasLayer
 
-var minutes = 1
+var is_timer = true
+
+var minutes = 0 
 var seconds = 0
-var is_running = false
+var decimals = 0
+
+var mode_label = 'Timer'
+
+func update_label():
+	$Clable.text = str(decimals) 
+	$TimeLabel.text = '%02d:%02d'%[minutes,seconds]
+
+func _on_mode_button_pressed():
+	if $MainTimer.is_stopped():
+		minutes = 0
+		seconds = 0
+		decimals = 0 
+		is_timer = !is_timer
+		if is_timer == true:
+			mode_label = 'TIMER'
+		else:
+			mode_label = 'STOPWATCH'
+	update_label()
+	$ModeLabel.text = mode_label
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_start_button_pressed():
+	if $MainTimer.is_stopped():
+		$MainTimer.start()
+		$StatusLabel.text = 'RUNNING'
+	else:
+		$MainTimer.stop()
+		$StatusLabel.text = 'STOPPED'
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_stopwatch():
+	decimals += 1
+	if decimals >= 10:
+		decimals = 0
+		seconds += 1
+	if seconds >= 60:
+		seconds = 0
+		minutes += 1
+	if minutes >= 59:
+		seconds = 59
+		minutes = 59
+		decimals = 9
+		$StatusLabel.text = 'STOPPED'
+		$MainTimer.stop()
+		$StopTimerSound.play()
+	update_label()
+
+func update_timer():
+	decimals -= 1
+	if decimals < 0:
+		decimals = 9
+		seconds -= 1
+	if seconds < 0:
+		seconds = 59
+		minutes -= 1
+	if minutes < 0:
+		minutes = 0
+		seconds = 0
+		decimals = 0
+		$StatusLabel.text = 'STOPPED'
+		$MainTimer.stop()
+	update_label()
+
+func _on_main_timer_timeout():
+	if is_timer == false:
+		update_stopwatch()
+	else:
+		update_timer()
+
+
+func _on_sec_button_pressed():
+	if $MainTimer.is_stopped() and is_timer == true:
+		seconds += 1
+		if seconds > 59:
+			seconds = 0
+		decimals = 0
+		update_label()
+ 
+
+
+func _on_min_button_pressed():
+	if $MainTimer.is_stopped() and is_timer == true:
+		minutes += 1
+		if minutes > 59:
+			minutes = 0
+		decimals = 0
+		update_label()
+
+
+func _on_clear_button_pressed():
+	if $MainTimer.is_stopped():
+		minutes = 0
+		seconds = 0
+		decimals = 0
+		update_label()
+
